3bae9e53302468b63efdd9999ecfac639b4b7295
ghostiek <patricktawil1@hotmail.com>
2018-03-12 16:57:38 -0700

    Refactored and added a Unit Test
    Thanks to
    https://codereview.stackexchange.com/questions/158347/library-for-the-summary-api-used-to-get-summaries-of-webpages

---
+++ SmmryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SmmryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SmmryTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8456576f-3208-44d4-b3c0-c185180f89e5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SmmryTest/SmmryParametersTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Smmry;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SmmryTest
+{
+    [TestClass]
+    public class SmmryParametersTest
+    {
+        [TestMethod]
+        public void GetSmmryParameters()
+        {
+            var smmryParam = new SmmryParameters()
+            {
+                ApiKey = "ApiKey",
+                Url = "https://en.wikipedia.org/wiki/Augustus",
+                SentenceCount = 3,
+                KeywordCount = 24,
+                IncludeBreaks = true,
+                IncludeQuotes = true
+            };
+            Assert.AreEqual("?SM_API_KEY=ApiKey&SM_LENGTH=3&SM_KEYWORD_COUNT=24&SM_WITH_BREAK=True&SM_QUOTE_AVOID=True&SM_URL=https://en.wikipedia.org/wiki/Augustus", smmryParam.ToString());
+        }
+    }
+}
